738cbfdecdb9932c1f7a4c4989d3c41afe0ccf09
Carvihi <cvida87@gmail.com>
2018-07-26 19:09:07 +0200

    forensic2

--- forensic/forensic.py
+++ forensic/forensic.py
@@ -2,36 +2,38 @@
 # -*- coding: utf-8 -*-
 
 characteristics_dict = {
-    "black": "CCAGCAATCGC", "Brown": "GCCAGTGCCG", "Blonde": "TTAGCTATCGC",
-    "Square": "GCCACGG", "Round": "ACCACAA", "Oval": "AGGCCTCA",
-    "Blue": "TTGTGGTGGC", "Green": "GGGAGGTGGC", "Dark": "AAGTAGTGAC",
-    "Female": "TGAAGGACCTTC", "Male": "TGCAGGAACTTC",
-    "White": "AAAACCTCA", "Black": "CGACTACAG", "Asian": "CGCGGGCCG"}
+    "hair_black": "CCAGCAATCGC", "hair_Brown": "GCCAGTGCCG", "hair_Blonde": "TTAGCTATCGC",
+    "face_Square": "GCCACGG", "face_Round": "ACCACAA", "face_Oval": "AGGCCTCA",
+    "eye_Blue": "TTGTGGTGGC", "eye_Green": "GGGAGGTGGC", "eye_Dark": "AAGTAGTGAC",
+    "gender_Female": "TGAAGGACCTTC", "gender_Male": "TGCAGGAACTTC",
+    "race_White": "AAAACCTCA", "race_Black": "CGACTACAG", "race_Asian": "CGCGGGCCG"}
 
 sample_file = open ("sample.txt", "r")
 dna = sample_file.read()
+sample_file.close()
 
 guilty = []
 for key, value in characteristics_dict.iteritems():
     if dna.find(value) != -1:
         guilty.append(key)
 
-sample_file.close()
 
 print "Guilty characteristics are: "
 for item in guilty:
     print "- " + item
 
 suspect_dict = {
-    "Eva": "[Female, White, Blonde, Blue, Oval]",
-    "Larisa": "[Female, White, Brown, Dark, Oval]",
-    "Matej": "[Male, White, Black, Blue, Oval]",
-    "Miha": "[Male, White, Brown, Green, Square]"
+    "Eva": "[gender_Female, race_White, hair_Blonde, eye_Blue, face_Oval]",
+    "Larisa": "[gender_Female, race_White, hair_Brown, eye_Dark, face_Oval]",
+    "Matej": "[gender_Male, race_White, hair_Black, eye_Blue, face_Oval]",
+    "Miha": "[gender_Male, race_White, hair_Brown, eye_Green, face_Square]"
 }
 
 for key, value in suspect_dict.iteritems():
     if all(elem in value for elem in guilty):
         print "The person who ate the chocolate ice-cream is " + key
+
+print ("_" * 51)
 
 print "Thank you to use our services."
 
